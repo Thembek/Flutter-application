517e138a872176b565fb8e9f9421b0eab39d4585
Andile <andile.goba@younglings.africa>
2022-09-16 18:13:08 +0200

    mongodb connection constant file commit

--- lib/backend/constant.dart
+++ lib/backend/constant.dart
@@ -1,2 +1,2 @@
-const MONGO_CONN_URL = "";
+const MONGO_CONN_URL = "mongodb+srv://abba:abba<password>@cluster0.fi9xkh8.mongodb.net/mydatabase?retryWrites=true&w=majority";
 const USER_COLLECTION = "user";
