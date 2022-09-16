0e1733d3429f09b60c91c6bb9145a2486bb8c74f
Andile <andile.goba@younglings.africa>
2022-09-16 18:24:00 +0200

    mongodb crud function commit

---
+++ lib/backend/mongodb.dart
@@ -0,0 +1,28 @@
+import 'dart:developer';
+import 'package:mongo_dart/mongo_dart.dart';
+import 'package:flutter_main_application/backend/constant.dart';
+import 'package:flutter_main_application/backend/mongoDbModel.dart';
+
+class MongoDatabase {
+  static var db, userCollection;
+  static connect() async {
+    db = await Db.create(MONGO_CONN_URL);
+    await db.open();
+    inspect(db);
+    userCollection = db.collection(USER_COLLECTION);
+  }
+
+  static Future<String> signup(MongoDbModel data) async {
+    try{
+      var result = await userCollection.insertOne(data.toJson());
+      if (result.isSuccess) {
+        return "Data has been inserted, successfully.";
+      } else {
+        return "Something went wrong, while you were inserting your data.";
+      }
+    } catch (e) {
+      print(e.toString());
+      return e.toString();
+    }
+  }
+}
