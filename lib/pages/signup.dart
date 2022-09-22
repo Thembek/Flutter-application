f75fa0f807d4cb767be1925950f02d8dd8359630
Andile <andile.goba@younglings.africa>
2022-09-22 17:41:46 +0200

    sign up page update commit

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -22,7 +22,60 @@ class _SignupState extends State<Signup> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/register.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 35, top: 30),
+              child: Text(
+                'Create an\nAccount',
+                style: GoogleFonts.pacifico(
+                  fontSize: 33,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.28,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(left: 35, right: 35),
+                      child: Column(
+                        children: [
+                          TextField(
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                border
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
